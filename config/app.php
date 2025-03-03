242684abc69c13f56ff7c7eff1c83de9dabb45a6
Nwankpele damilola <28966589+lohun@users.noreply.github.com>
2025-03-03 18:01:13 +0100

    test slash

--- config/app.php
+++ config/app.php
@@ -164,12 +164,11 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        Kyslik\ColumnSortable\ColumnSortableServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
         /*
         * Third Party Service Providers...
         */
