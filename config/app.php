f52444191a15f088558622d4f222081eb0525127
Nwankpele damilola <28966589+lohun@users.noreply.github.com>
2025-03-03 18:01:13 +0100

    test slash

--- config/app.php
+++ config/app.php
@@ -164,16 +164,16 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        Kyslik\ColumnSortable\ColumnSortableServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
         /*
         * Third Party Service Providers...
         */
-        Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class
+        \Kyslik\ColumnSortable\ColumnSortableServiceProvider::class,
+        \Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class
     ])->toArray(),
 
     /*
